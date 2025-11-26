a8e9e94e1d29559d953c88b47fadaa8eb7827269
ushalbansode-ai <ushalbansode@gmail.com>
2025-11-26 15:49:14 +0530

    Update fetch_bhavcopy.py

--- src/fetch_bhavcopy.py
+++ src/fetch_bhavcopy.py
@@ -1,1 +1,19 @@
+import requests
+import datetime
 
+def fetch_dat():
+    today = datetime.datetime.now().strftime("%d%m%Y")
+    url = f"https://www.nseindia.com/content/historical/DERIVATIVES/{today}/fo{today}bhav.csv.zip"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    print(f"Downloading: {url}")
+    r = requests.get(url, headers=headers)
+
+    if r.status_code != 200:
+        raise Exception("Failed to download DAT bhavcopy")
+
+    return r.content
+  
